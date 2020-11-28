bf0630f74d09670516486bcb62ee9826a96cab5d
kenjiuno <h8mastre@gmail.com>
2020-11-28 15:26:02 +0900

    Adding UTF7 samples.

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ async function proceedAll() {
     await proceedAsync('utf8Subject.eml');
     await proceedAsync('iso2022jpBody.eml');
     await proceedAsync('iso2022jpSubject.eml');
+    await proceedAsync('utf7Body.eml');
+    await proceedAsync('utf7Subject.eml');
 }
 
 async function proceedAsync(emlFile) {
-    console.log(`Proceeding ${emlFile}`);
+    console.log(`--- Proceeding ${emlFile}`);
 
     const source = await fs.promises.readFile(emlFile);
 
     const parsed = await simpleParser(source);
 
-    console.log(parsed && parsed.subject);
+    console.log(`  Subject: ${parsed && parsed.subject}`);
+    console.log(`  Message: ${parsed && parsed.text}`);
 }
